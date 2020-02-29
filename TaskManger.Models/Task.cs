28641a32fef859242c134fb693193530548cc99f
fiqriismail <fiqri@peachit.lk>
2020-02-29 11:02:44 +0530

    Add project files.

---
+++ TaskManger.Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManger.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TaskStatus Status { get; set; } // 0 - New, 1 - InProgress, 2 - Completed
+        public DateTime Created { get; set; }
+        public DateTime Due { get; set; }
+        public DateTime Completed { get; set; }
+
+        public int OwnerId { get; set; } = 1;
+    }
+
+    public enum TaskStatus
+    {
+        New,
+        InProgress,
+        Completed
+    }
+}
---
+++ TaskManger.Web.API/Controllers/TaskController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManger.Web.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("You have 12 tasks today");
+        }
+    }
+}
